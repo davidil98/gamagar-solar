d628b5f99fcb7fdf3c397684fb37e89b3bad8964
David Ibarra <david.ibarral0798@gmail.com>
2026-01-26 12:47:12 -0600

    Generated static version.

--- freeze.py
+++ freeze.py
@@ -1,5 +1,6 @@
 from app import app
 from flask_frozen import Freezer
+import os
 
 app.config['FREEZER_DESTINATION'] = 'docs'
 app.config['FREEZER_RELATIVE_URLS'] = True
@@ -8,3 +9,7 @@ freezer = Freezer(app)
 
 if __name__ == "__main__":
     freezer.freeze()
+
+    cname_path = os.path.join('docs', 'CNAME')
+    with open(cname_path, 'w') as f:
+        f.write('gamagarsolar.com.mx')
